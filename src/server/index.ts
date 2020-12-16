8c016e6959a4ff053e8bface9cc4fe960268eef1
Quramy <yosuke.kurami@gmail.com>
2020-12-16 21:02:11 +0900

    modify

--- src/server/index.ts
+++ src/server/index.ts
@@ -45,37 +45,37 @@ app.post('/graphql', async (req, res) => {
     return;
   }
   try {
-    const result = await execute({
-      schema,
-      document,
-      variableValues: variables ?? {},
-    });
-    if (isAsyncIterable(result)) {
-      res.writeHead(200, {
-        'Content-Type': `multipart/mixed; charset=UTF-8; boundary="${BOUNDARY}"`,
-        'Transfer-Encoding': 'chunked',
-      });
-      res.write(CRLF + CRLF + `--${BOUNDARY}`);
-      for await (const payloadObj of result) {
-        const payloadBody = JSON.stringify(payloadObj);
-        // prettier-ignore
-        const multipart = CRLF
-                        + 'Content-Type: application/json; charset=UTF-8' + CRLF
-                        + `Content-Length: ${payloadBody.length}` + CRLF
-                        + CRLF
-                        + payloadBody
-                        + CRLF
-                        + CRLF
-                        + `--${BOUNDARY}`
-        res.write(multipart);
-        // @ts-expect-error
-        typeof res.flush === 'function' && res.flush !== ServerResponse.prototype.flush && res.flush();
-      }
-      res.write('--' + CRLF);
-      res.end();
-    } else {
-      res.json(result).end();
-    }
+const result = await execute({
+  schema,
+  document,
+  variableValues: variables ?? {},
+});
+if (isAsyncIterable(result)) {
+  res.writeHead(200, {
+    'Content-Type': `multipart/mixed; charset=UTF-8; boundary="${BOUNDARY}"`,
+    'Transfer-Encoding': 'chunked',
+  });
+  res.write(CRLF + CRLF + `--${BOUNDARY}`);
+  for await (const payloadObj of result) {
+    const payloadBody = JSON.stringify(payloadObj);
+    // prettier-ignore
+    const multipart = CRLF
+                    + 'Content-Type: application/json; charset=UTF-8' + CRLF
+                    + `Content-Length: ${payloadBody.length}` + CRLF
+                    + CRLF
+                    + payloadBody
+                    + CRLF
+                    + CRLF
+                    + `--${BOUNDARY}`
+    res.write(multipart);
+    // @ts-expect-error
+    typeof res.flush === 'function' && res.flush !== ServerResponse.prototype.flush && res.flush();
+  }
+  res.write('--' + CRLF);
+  res.end();
+} else {
+  res.json(result).end();
+}
   } catch (e) {
     const message = e instanceof Error ? e.message : 'internal server error';
     return res
